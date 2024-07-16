75f50eb904f23b385098e7fe7e956b6ce6dfe785
SirSaleh <animatorsaleh@gmail.com>
2024-07-16 18:36:31 +0330

    Add order_instance table

--- Postgres/0 - structure_define/define_simple_shop.sql
+++ Postgres/0 - structure_define/define_simple_shop.sql
@@ -6,16 +6,30 @@ CREATE TABLE IF NOT EXISTS user_instance (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(32),
     last_name VARCHAR(32),
-    password_hash VARCHAR(32)
+    password_hash VARCHAR(64)
 );
 
 -- Product Table
 
 CREATE TABLE IF NOT EXISTS product (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(32) NOT NULL,
     desc_text TEXT NULL,
     price DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Order Table
+
+CREATE TABLE IF NOT EXISTS order_instance (
+    id Serial PRIMARY KEY,
+    customer_note  Text,
+
+    user_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user_instance(id),
+    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id)
+);
