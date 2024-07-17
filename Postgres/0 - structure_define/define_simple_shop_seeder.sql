bad99cacc8dc2e87d20c9383a9371894387c8efd
SirSaleh <animatorsaleh@gmail.com>
2024-07-17 23:13:12 +0330

    Add user_instace seeder

---
+++ Postgres/0 - structure_define/define_simple_shop_seeder.sql
@@ -0,0 +1,32 @@
+-- Create Users
+
+INSERT INTO user_instance (first_name, last_name, username, password_hash) 
+    VALUES
+    ('David', 'Jefferson', 'david.jefferson', MD5('David_is_Capperfiled')),
+    ('Mansoor', 'Terello', 'mansoor.terello', MD5('TechTechTech')),
+    ('Daivd', 'Angelo', 'daivd.angelo', MD5('Machintosh')),
+    ('Nelson', 'Davari', 'nelson.davari', MD5('Tech')),
+    ('Tim', 'Besti', 'tim.besti', MD5('Tech')),
+    ('Moolan', 'Techno', 'moolan.techno', MD5('Tolstoy')),
+    ('Dorothy', 'Wright', 'dorothy.wright', MD5('wrightBrothers')),
+    ('Kimberly', 'Lopez', 'kimberly.lopez', MD5('latinPop')),
+    ('Donna', 'Hill', 'donna.hill', MD5('hillTop')),
+    ('Carol', 'Scott', 'carol.scott', MD5('scotty')),
+    ('Michelle', 'Adams', 'michelle.adams', MD5('michelle2021')),
+    ('Emily', 'Baker', 'emily.baker', MD5('bakeCake')),
+    ('Helen', 'Gonzalez', 'helen.gonzalez', MD5('gonzalesSpeed')),
+    ('Deborah', 'Nelson', 'deborah.nelson', MD5('nelsonMandela')),
+    ('Laura', 'Carter', 'laura.carter', MD5('cartRider')),
+    ('Elizabeth', 'Mitchell', 'elizabeth.mitchell', MD5('lostIsland')),
+    ('Maria', 'Perez', 'maria.perez', MD5('perezHilton')),
+    ('Susan', 'Roberts', 'susan.roberts', MD5('robertDeNiro')),
+    ('Lisa', 'Turner', 'lisa.turner', MD5('turnAround')),
+    ('Nancy', 'Phillips', 'nancy.phillips', MD5('phillipTheGreat')),
+    ('Karen', 'Campbell', 'karen.campbell', MD5('campingLife')),
+    ('Betty', 'Parker', 'betty.parker', MD5('spiderman')),
+    ('Margaret', 'Evans', 'margaret.evans', MD5('evansFamily')),
+    ('Sandra', 'Edwards', 'sandra.edwards', MD5('edwardScissorhands')),
+    ('Ashley', 'Collins', 'ashley.collins', MD5('collinFirth')),
+    ('Dorothy', 'Stewart', 'dorothy.stewart', MD5('stewartLittle')),
+    ('Kimberly', 'Sanchez', 'kimberly.sanchez', MD5('dirtyDancing')),
+    ('Donna', 'Morris', 'donna.morris', MD5('morrisMinor'));
